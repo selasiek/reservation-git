0ba8d90ea73b4d18ed6d79a7fc564d6a4e9c1e11
selasiek <kitsipaul@ymail.com>
2017-11-18 20:21:34 +0000

    fixed schedules migration

--- database/migrations/2017_07_10_123423_create_schedules_table.php
+++ database/migrations/2017_07_10_123423_create_schedules_table.php
@@ -19,8 +19,8 @@ class CreateSchedulesTable extends Migration
             $table->string('from');
             $table->string('to');
             $table->date('date');
-            $table->date('seat_count');
-            $table->date('seats_sold');
+            $table->date('seat_count')->unsigned()->default(0);
+            $table->date('seats_sold')->unsigned()->default(0);
             $table->string('fare');
             $table->string('reporting');
             $table->string('departure');
